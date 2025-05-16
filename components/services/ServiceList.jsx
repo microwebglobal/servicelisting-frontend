5065f8aaef2748345878b4da62fa918cf501716a
Pavan Silva <119662370+Pavan-Silva@users.noreply.github.com>
2025-05-16 16:09:27 +0530

    addes view details functionality in service list

--- components/services/ServiceList.jsx
+++ components/services/ServiceList.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { serviceAPI } from "@/api/services";
 import { useToast } from "@/hooks/use-toast";
-import { AlertCircleIcon, ChevronDown, Star } from "lucide-react";
+import { AlertCircleIcon, ChevronDown, ChevronRight, Star } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import DOMPurify from "dompurify";
 import Modal from "react-modal";
@@ -14,7 +14,9 @@ export function ServiceList({ typeId, cityId, addToCart }) {
   const [loading, setLoading] = useState(true);
   const { toast } = useToast();
   const [isOpen, setIsOpen] = useState(false);
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
   const [modalOverview, setModelOverview] = useState(null);
+  const [serviceDetail, setServiceDetail] = useState(null);
 
   const openModal = (overview) => {
     setModelOverview(overview);
@@ -101,7 +103,9 @@ export function ServiceList({ typeId, cityId, addToCart }) {
 
                 <div className="space-y-0.5">
                   <p className="font-medium text-sm">{service.name}</p>
-                  <p className="text-sm text-gray-500">{service.description}</p>
+                  <p className="text-sm text-gray-500 line-clamp-2">
+                    {service.description}
+                  </p>
                 </div>
               </div>
 
@@ -111,9 +115,12 @@ export function ServiceList({ typeId, cityId, addToCart }) {
             {serviceItems[service.service_id] && (
               <div className="grid grid-cols-1 gap-4">
                 {serviceItems[service.service_id].map((item) => (
-                  <div key={item.item_id} className="bg-muted p-4 rounded-lg">
+                  <div
+                    key={item.item_id}
+                    className="bg-muted p-4 rounded-lg space-y-2"
+                  >
                     <div className="flex justify-between items-start">
-                      <div>
+                      <div className="space-y-1">
                         <h6
                           className="font-medium"
                           onClick={() => openModal(item.overview)}
@@ -121,9 +128,27 @@ export function ServiceList({ typeId, cityId, addToCart }) {
                           {item.name}
                         </h6>
 
-                        <p className="text-sm text-muted-foreground mt-1">
-                          {item.description}
-                        </p>
+                        <div className="space-y-3">
+                          <p className="text-sm text-muted-foreground">
+                            {item.description}
+                          </p>
+
+                          {item.is_home_visit && (
+                            <p className="text-sm text-[#5f60b9] flex items-start">
+                              <AlertCircleIcon className="mr-2 w-4" />
+                              You need to visit service provider to get this
+                              service
+                            </p>
+                          )}
+
+                          {item.advance_percentage > 0 && (
+                            <p className="text-[#5f60b9] flex items-start text-sm">
+                              <AlertCircleIcon className="mr-2 w-4" />
+                              {item.advance_percentage}% Advanced Payment
+                              Required
+                            </p>
+                          )}
+                        </div>
                       </div>
 
                       <div className="text-right">
@@ -154,19 +179,17 @@ export function ServiceList({ typeId, cityId, addToCart }) {
                       </div>
                     </div>
 
-                    {item.is_home_visit && (
-                      <p className="text-sm text-[#5f60b9] flex items-start mt-6">
-                        <AlertCircleIcon className="mr-2 w-4" />
-                        You need to visit service provider to get this service
-                      </p>
-                    )}
-
-                    {item.advance_percentage > 0 && (
-                      <p className="text-[#5f60b9] flex items-start text-sm mt-6">
-                        <AlertCircleIcon className="mr-2 w-4" />
-                        {item.advance_percentage}% Advanced Payment Required
-                      </p>
-                    )}
+                    <Button
+                      variant="ghost"
+                      className="text-[#5f60b9] py-0 px-1 font-semibold"
+                      onClick={() => {
+                        setIsDetailsOpen(true);
+                        setServiceDetail(item.overview);
+                      }}
+                    >
+                      View details
+                      <ChevronRight />
+                    </Button>
                   </div>
                 ))}
               </div>
@@ -179,12 +202,32 @@ export function ServiceList({ typeId, cityId, addToCart }) {
           onRequestClose={closeModal}
           ariaHideApp={false}
           contentLabel="Service Description"
-          className="m-10 bg-white p-8 rounded-md shadow-xl transform transition-all duration-300 ease-in-out w-2/4 overflow-y-auto max-h-[80vh]"
+          className="m-10 bg-white p-8 rounded-md shadow-xl transform transition-all duration-300 ease-in-out w-2/4 overflow-y-auto max-h-[80vh] max-w-md"
           overlayClassName="fixed inset-0 flex justify-center items-center bg-opacity-50 bg-black backdrop-blur-xs"
         >
           <div
             dangerouslySetInnerHTML={{
               __html: DOMPurify.sanitize(modalOverview),
+            }}
+          />
+        </Modal>
+
+        <Modal
+          isOpen={isDetailsOpen}
+          onRequestClose={() => {
+            setIsDetailsOpen(false);
+            setServiceDetail("");
+          }}
+          ariaHideApp={false}
+          contentLabel="Service Description"
+          className="m-10 bg-white p-8 rounded-md shadow-xl transform transition-all duration-300 ease-in-out w-2/4 overflow-y-auto max-h-[80vh] max-w-md"
+          overlayClassName="fixed inset-0 flex justify-center items-center bg-opacity-50 bg-black backdrop-blur-xs"
+        >
+          <h4 className="text-xl font-bold mb-4">Service Description</h4>
+
+          <div
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(serviceDetail),
             }}
           />
         </Modal>
